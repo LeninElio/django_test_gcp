f41fd11f07438aa668dca2b57ae0dbf2ae8e87f7
Lenin Elio <moreno_leninelio@hotmail.es>
2024-07-17 16:03:39 -0500

    Add supervisord

--- test_app/views.py
+++ test_app/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def home_view(request):
-    return HttpResponse('Hola, esta es una app de prueba para GCP...')
+    return HttpResponse('Hola desde WSL, esta es una app de prueba para GCP ...')
